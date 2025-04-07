e7d3088483cdbf12ac7dff7ab7960668dc12b4b1
Suraj Vinod Mule <167108148+surajmule400@users.noreply.github.com>
2025-04-07 13:42:42 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-print("Hello, GitHub! Welcome to this simple Python project, my name is suraj")
+print("Hello, GitHub! Welcome to this simple Python project, my name is suraj and i am looking happy")
